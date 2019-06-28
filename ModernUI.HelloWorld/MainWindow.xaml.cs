75f447fdf34a37c6aaac90ec1f3ae7917d47e082
fengyhack <fengyhack@gmail.com>
2019-06-28 15:31:08 +0800

    examples collected

---
+++ ModernUI.HelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using FirstFloor.ModernUI.Windows.Controls;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ModernUI.HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ModernWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Run_MouseEnter(object sender, MouseEventArgs e)
+        {
+            popLink.IsOpen = true;
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(((Hyperlink)sender).NavigateUri.ToString());
+        }
+
+        private void ShowMyDialog_Click(object sender, RoutedEventArgs e)
+        {
+            MyModernDialog dialog = new MyModernDialog();
+            dialog.ShowDialog();
+        }
+    }
+}
---
+++ ProWPF.Effects/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Expression.Media.Effects;
+
+namespace ProWPF.Effects
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MyButton_Loaded(object sender, RoutedEventArgs e)
+        {
+            MagnifyEffect effect = new MagnifyEffect();
+            effect.Amount = 30;
+            MyButton.Effect = effect;
+        }
+    }
+}
